401ea20d2784a34f1b42aeb0bb447b98dc5bfe49
natibogale45@gmail.com <natibogale45@gmail.com>
2022-06-03 06:59:34 +0300

    connection on the sqlite had problems, and fixed that

--- eventTray/settings.py
+++ eventTray/settings.py
@@ -142,7 +142,7 @@ WSGI_APPLICATION = "eventTray.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'db.sqlite3',
     }
 }
 
