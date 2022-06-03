f5f93e99f9a2879a0568846b39b689cb59a63376
natibogale45@gmail.com <natibogale45@gmail.com>
2022-06-03 05:18:23 +0300

    Configured django to host on heroku for api developement, database connection still localhost, debug is true

--- eventTray/settings.py
+++ eventTray/settings.py
@@ -42,7 +42,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.0.9", "127.0.0.1", "*","eventtray-api"]
+ALLOWED_HOSTS = ["192.168.0.9", "127.0.0.1", "*","eventtray-api.heroku.com"]
 
 AUTH_USER_MODEL = "authentication.User"
 
