782c27683a7e886fb77e757bb7cafdfe5db119b8
natibogale45@gmail.com <natibogale45@gmail.com>
2022-06-07 04:18:26 +0300

    Moved the api views to it's own views module

--- authentication/api_urls.py
+++ authentication/api_urls.py
@@ -1,23 +1,11 @@
 from django.urls import path, include
-from .views import *
 from .api_views import *
 
 urlpatterns = [
-
-    #All authentication urls api
+    # path("", APIOverview.as_view(), name="home"),
     path("api/auth/create/", UserRegisterView.as_view(), name="add-user"),
     path("api/profile/", UserProfileView.as_view(), name="view-profile"),
     path("api/auth/otp/", sendOtp.as_view(), name="send-otp"),
     path("api/auth/verify/", VerifyOTP.as_view(), name="verify-otp"),
 
-
-
-
-
-
-    # Organizer no api urls
-
-    path("", home, name="home"),
-    path("verify/otp", verifyOtp, name="verify-otp"),
-    path('organizer/', include('organizer.urls')),
 ]
